529008d34caf8e5efcad4adf6bb8efe0f688ffae
Miroslav Krastev <53656273+mirokrastev@users.noreply.github.com>
2026-06-22 20:53:01 +0300

    Safely bump npm packages. Fix username min & max length validations

--- src/pages/Auth/Register/schema.js
+++ src/pages/Auth/Register/schema.js
@@ -14,8 +14,8 @@ export const RegisterSchema = yup.object().shape({
   username: yup
     .string()
     .required("Username is required")
-    .min(6, "Username must be at least 3 characters")
-    .max(20, "Username must be at most 150 characters"),
+    .min(3, "Username must be at least 3 characters")
+    .max(20, "Username must be at most 20 characters"),
   password: yup
     .string()
     .required("Password is required")
